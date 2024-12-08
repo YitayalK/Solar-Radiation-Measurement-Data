23344b88958054198885349e7c93140ee457b886
Yitayal Kassie <yitayal4all@gmail.com>
2024-12-08 15:08:34 +0300

    Statistical ata

--- scripts/Exploratory_Data_Analysis.py
+++ scripts/Exploratory_Data_Analysis.py
@@ -1,8 +1,9 @@
 import pandas as pd # importing pandas
-df = pd.read_csv("D:\File Pack\Courses\10Acadamey\Kifiya AIM Week 0\data/benin-malanville.csv") # reading CSV file
+df = pd.read_csv("D:/File Pack/Courses/10Acadamey/Kifiya AIM Week 0/data/benin-malanville.csv") # reading CSV file
 
 summary_statistics = df.describe() #Calculate summary statistics
 
 
-print(summary_statistics) # Print the summary statistics
+print(summary_statistics) # Print the summary statisti
+
 
