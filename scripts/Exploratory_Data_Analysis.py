57f46fcc1754d076196063f372609bd48070101d
Yitayal Kassie <yitayal4all@gmail.com>
2024-12-08 14:36:21 +0300

    Statistical ata

---
+++ scripts/Exploratory_Data_Analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd # importing pandas
+df = pd.read_csv("D:\File Pack\Courses\10Acadamey\Kifiya AIM Week 0\data/benin-malanville.csv") # reading CSV file
+
+summary_statistics = df.describe() #Calculate summary statistics
+
+
+print(summary_statistics) # Print the summary statistics
+
